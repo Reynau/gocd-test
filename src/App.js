b465da6c4f3eead9f553ec7b609af910760f3240
Reynau <xavi.lopez.reynau@gmail.com>
2020-04-16 11:16:09 +0200

    Commit to trigger CI and fail tests

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Different Text to Fail Test
         </a>
       </header>
     </div>
